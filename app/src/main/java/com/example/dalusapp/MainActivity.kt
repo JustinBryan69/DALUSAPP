386cdf08bc62d2ad5ea9323be597fd0ba5e644eb
JustinBryan69 <jtin253@gmail.com>
2022-10-18 22:13:55 +0800

    FIRST COMMIT

---
+++ app/src/main/java/com/example/dalusapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.dalusapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@MainActivity, Home::class.java)
+            startActivity(intent)
+        },2000)
+    }
+
+}
